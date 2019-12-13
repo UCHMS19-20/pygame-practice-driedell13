9948d287d0028ade96fd738e7df5f74aa870238c
david <driedell@ucvts.tec.nj.us>
2019-12-13 10:47:48 -0500

    pygame color practice

---
+++ colors.py
@@ -0,0 +1,24 @@
+import sys
+import pygame
+
+# Initialize pygame so it runs in the background and manages things
+pygame.init()
+
+# Create a display. Size must be a tuple, which is why it's in parentheses
+screen = pygame.display.set_mode( (400, 300) )
+
+#colors
+red = pygame.Color(255,0,0)
+orange = pygame.Color(255,128,0)
+
+screen.fill(orange)
+pygame.display.flip()
+
+# Main loop. Your game would go inside this loop
+while True:
+    # do something for each event in the event queue (list of things that happen)
+    for event in pygame.event.get():
+        # Check to see if the current event is a QUIT event
+        if event.type == pygame.QUIT:
+            # If so, exit the program
+            sys.exit()
